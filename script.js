4b197dcf33c254987ca933dbcac37a77cf2d73b2
Elias Neves <elias2015neves2016@gmail.com>
2023-12-07 18:42:38 -0300

    Nv

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('playButton').addEventListener('click', function() {
+    // Redireciona para outra página
+    window.location.href = 'index.html'; // Substitua 'outra_pagina.html' pelo caminho da página para onde você quer redirecionar
+  });
+  
