1095a7be7e1351dfadbaeba3bbe877afd3d5e11a
Elias Neves <elias2015neves2016@gmail.com>
2023-12-07 19:13:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('playButton').addEventListener('click', function() {
     // Redireciona para outra página
-    window.location.href = 'index.html'; // Substitua 'outra_pagina.html' pelo caminho da página para onde você quer redirecionar
+    window.location.href = 'cartão.html'; // Substitua 'outra_pagina.html' pelo caminho da página para onde você quer redirecionar
   });
   
