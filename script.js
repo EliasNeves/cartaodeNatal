67088f81c45f53b49d26be0f06cd2347e1fe0018
Elias Neves <elias2015neves2016@gmail.com>
2023-12-07 22:07:02 -0300

    Again

--- script.js
+++ script.js
@@ -3,24 +3,3 @@ document.getElementById('playButton').addEventListener('click', function() {
     window.location.href = 'cartão.html'; // Substitua 'outra_pagina.html' pelo caminho da página para onde você quer redirecionar
   });
   
-  const prevButton = document.querySelector('.prevButton');
-const nextButton = document.querySelector('.nextButton');
-const gallery = document.querySelector('.gallery');
-
-let translateX = 0;
-
-nextButton.addEventListener('click', () => {
-  translateX -= 100;
-  if (translateX < -(gallery.children.length - 1) * 100) {
-    translateX = 0;
-  }
-  gallery.style.transform = `translateX(${translateX}%)`;
-});
-
-prevButton.addEventListener('click', () => {
-  translateX += 100;
-  if (translateX > 0) {
-    translateX = -(gallery.children.length - 1) * 100;
-  }
-  gallery.style.transform = `translateX(${translateX}%)`;
-});
